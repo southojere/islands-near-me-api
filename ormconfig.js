d6617f53379c12512e47e9379628fd7921ce76e5
Jeremy <jeremy.southon@rocketlab.com.au>
2020-05-17 18:33:38 +1000

    updated ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,18 @@
 module.exports = [
   {
     name: "development",
-    type: "sqlite",
-    database: "database.sqlite",
     synchronize: true,
     logging: true,
+
+    type: "sqlite",
+    database: "database.sqlite",
+    // example using database hosted from heroku.
+    // type: "postgres",
+    // url:
+    //   "postgres://examplehosturi.compute-1.amazonaws.com:5432/examplepassword",
+    // extra: {
+    //   ssl: true
+    // },
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
     subscribers: ["src/subscriber/**/*.ts"],
@@ -18,7 +26,7 @@ module.exports = [
     name: "production",
     type: "postgres",
     url: process.env.DATABASE_URL,
-    synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
+    synchronize: false, // switch this to false once you have the initial tables created and use migrations instead
     logging: false,
     entities: ["dist/entity/**/*.js"],
     migrations: ["dist/migration/**/*.js"],
